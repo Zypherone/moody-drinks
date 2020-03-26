06b12f8e518b69efc31d7e8ac213aefb1726560e
Dan Tucker-Saunders <danutuckersaunders@email.com>
2020-03-26 19:48:59 +1100

    Updated temp firebase code

--- _DT_app.js
+++ _DT_app.js
@@ -12,8 +12,17 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
+
+  // Get element
+  const preObject = document.getElementById('object');
+
+  // Create reference
+  const dbRefObject = firebase.database().ref().child('object');
+
+  // Sync object changes
+  dbRefObject.on('value', snap => console.log(snap.val()));
 
 
 
-})
+
+}());
