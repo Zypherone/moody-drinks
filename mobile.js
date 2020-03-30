cb460c84f87f403450b0789b57e6da19ae2ad934
Dan Tucker-Saunders <danutuckersaunders@email.com>
2020-03-30 21:11:49 +1100

    added new mobile landing page with slider

--- mobile.js
+++ mobile.js
@@ -1,10 +1,12 @@
 
 
-var pageLanding = $('<section>').html();
+var pageLanding = $('section').html();
 
 var template = Handlebars.compile(pageLanding);
   // execute the compiled template and print the output to the console
 
-  template({ BODY_CONTENT: "rocks!" })
-pageLanding.html();
+var temp =   template({ BODY_CONTENT: "rocks!" })
+
+$('section').html(temp);
+//pageLanding.html(temp);
   
